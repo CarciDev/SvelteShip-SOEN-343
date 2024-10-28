d4f2425533aeb9c0dab6e64d525c6f3e79f759b8
Brian Tkatch <brian@briantkatch.com>
2024-10-27 21:26:20 -0400

    Untrack .vercel

--- .vercel/output/static/_app/immutable/chunks/index.B2vNnDbs.js
+++
@@ -1,3 +0,0 @@
-import{z as g,A as u}from"./scheduler.DdsthWez.js";function C(t){return t*t*t}function x(t){const n=t-1;return n*n*n+1}function S(t,{delay:n=0,duration:o=400,easing:s=g}={}){const c=+getComputedStyle(t).opacity;return{delay:n,duration:o,easing:s,css:a=>`opacity: ${a*c}`}}function U(t,{delay:n=0,duration:o=400,easing:s=x,x:c=0,y:a=0,opacity:f=0}={}){const r=getComputedStyle(t),e=+r.opacity,y=r.transform==="none"?"":r.transform,p=e*(1-f),[l,m]=u(c),[$,d]=u(a);return{delay:n,duration:o,easing:s,css:(i,b)=>`
-			transform: ${y} translate(${(1-i)*l}${m}, ${(1-i)*$}${d});
-			opacity: ${e-p*b}`}}export{U as a,C as b,x as c,S as f};
--- .vercel/output/static/_app/immutable/entry/start.BF8Fjven.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"../chunks/entry.X8yMTra0.js";export{a as start};
