5c69f7fcadb4d5ca807f7a800af2045c7a784489
Brian Tkatch <brian@briantkatch.com>
2024-10-30 12:23:36 -0400

    Add Box and BoxRepository

--- src/lib/domain/Box.ts
+++ src/lib/domain/Box.ts
@@ -1,39 +1,27 @@
-import type { PrismaClient } from "@prisma/client";
-
 export class Box {
   id?: number;
+  createdAt?: Date;
+  updatedAt?: Date;
   heightCm: number;
   widthCm: number;
   depthCm: number;
   weightG: number;
 
   constructor(params: {
+    id?: number;
+    createdAt?: Date;
+    updatedAt?: Date;
     heightCm: number;
     widthCm: number;
     depthCm: number;
     weightG: number;
   }) {
+    this.id = params.id;
+    this.createdAt = params.createdAt;
+    this.updatedAt = params.updatedAt;
     this.heightCm = params.heightCm;
     this.widthCm = params.widthCm;
     this.depthCm = params.depthCm;
     this.weightG = params.weightG;
-  }
-
-  persist(prisma: PrismaClient) {
-    prisma.box.upsert({
-      where: { id: this.id },
-      update: {
-        heightCm: this.heightCm,
-        widthCm: this.widthCm,
-        depthCm: this.depthCm,
-        weightG: this.weightG,
-      },
-      create: {
-        heightCm: this.heightCm,
-        widthCm: this.widthCm,
-        depthCm: this.depthCm,
-        weightG: this.weightG,
-      },
-    });
   }
 }
